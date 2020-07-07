6cd55de768ef7d62805e46258ac462c278495556
Hanlarious <67278162+Hanlarious@users.noreply.github.com>
2020-07-06 23:30:16 -0400

    165. Merge Two Sorted Lists added

--- 165. Merge Two Sorted Lists/src/Solution.java
+++ 165. Merge Two Sorted Lists/src/Solution.java
@@ -10,22 +10,27 @@ public class Solution {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
-            dummy = l1;
-        while(l1 != null && l2 != null){
-            if (l2.val < l1.val){
-                l2.next = l1;
-                l2 = l2.next;
-                l1 = l1.next;
-            }
-            else if(l2.val > l1.val && l2.val < l1.next.val){
-                l2.next = l1.next;
-                l1.next = l2;
-                l2 = l2.next;
-                l1 = l1.next;
-            }
+        ListNode last = dummy;
 
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                last.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                last.next = l2;
+                l2 = l2.next;
+            }
+            last = last.next;
+        }
+        if(l1 != null){
+            last.next = l1;
+        }
+        else {
+            last.next = l2;
         }
         return dummy.next;
+
     }
 
 }
