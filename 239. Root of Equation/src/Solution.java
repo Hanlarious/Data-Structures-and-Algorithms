958a303f855a8a50e2d474cb4ec887e3699b537a
Hanlarious <67278162+Hanlarious@users.noreply.github.com>
2020-07-15 00:39:24 -0400

    366. Fibonacci added

---
+++ 239. Root of Equation/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    /*
+     * @param a: parameter of the equation
+     * @param b: parameter of the equation
+     * @param c: parameter of the equation
+     * @return: a double array, contains at most two root
+     */
+    public double[] rootOfEquation(double a, double b, double c) {
+        // write your code here
+    }
+
+}
---
+++ 366. Fibonacci/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /**
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
+    public int fibonacci(int n) {
+         int a = 0;
+         int b = 1;
+         for (int i = 0; i < n-1; i++){
+             int c = a + b;
+             a = b;
+             b = c;
+         }
+         return a;
+}
+}
