440cc1c075ae60d469a5e6691141db65891c5587
Hanlarious <67278162+Hanlarious@users.noreply.github.com>
2020-07-16 02:37:08 -0400

    239. Root of Equation added

--- 239. Root of Equation/src/Solution.java
+++ 239. Root of Equation/src/Solution.java
@@ -6,7 +6,28 @@ public class Solution {
      * @return: a double array, contains at most two root
      */
     public double[] rootOfEquation(double a, double b, double c) {
-        // write your code here
+        if ((b * b - 4 * a * c) < 0){
+            return new double[0];
+        }
+        if ((b * b - 4 * a * c) == 0){
+            double[] root = new double[1];
+            root[0] = -b / (2 * a);
+            return root;
+        }
+        else {
+            double[] root = new double[2];
+            double delta = Math.sqrt(b * b - 4 * a * c);
+            root[0] = (-b + delta) / (2 * a);
+            root[1] = (-b - delta) / (2 * a);
+
+            if (root[0] > root[1]){
+                double temp = root[0];
+                root[0] = root[1];
+                root[1] = temp;
+            }
+            return root;
+        }
+
     }
 
 }
